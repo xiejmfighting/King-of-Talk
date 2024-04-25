f7b25e3fc1e2ee883d0a3063fab33103e7a8a5c9
xiejiamei <1976178744@qq.com>
2024-04-25 17:19:40 +0800

    solve cross

--- kot_web/src/App.vue
+++ kot_web/src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    {{ content }}
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "App",
+  setup: () => {
+    let content = ref("");
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+        content.value = resp
+      }
+    })
+    return { content }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("./assets/background.jpg");
+  background-size: cover;
 }
 </style>
